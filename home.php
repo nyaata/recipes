c0d33b4dfeaff908eeb082d93eddb2872f635f08
nyaata <tessnyaata@gmail.com>
2022-01-24 09:23:00 +0300

    add the file

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Simple CRUD</title>
+</head>
+<h3>Lets cook </h3>
+
+<a href="login.php" class="button" style="vertical-align:bottom;"/>Login </a></div>
+
+<!-- javascript script to notifies delete one data  -->
+
+<!-- Color the website background -->
+<body style="background-color:powderblue;">
+
+<center>
+
+    
+</center>
+</body>
+</html>
