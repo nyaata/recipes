f3a17bd20a7ac99bfd2e95147e7f74b5f8918d82
nyaata <tessnyaata@gmail.com>
2022-01-24 09:14:45 +0300

    Recipe CRUD form

---
+++ dashboard2.php
@@ -0,0 +1,73 @@
+<?php
+include_once 'config.php';
+$result = mysqli_query($conn,"SELECT * FROM recipe");
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+ <title> View Recipes</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        
+    </style>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<body style="background-color:powderblue;">
+<body>
+
+  <a href="createrecipe.php"><button class="btn btn-primary"> Create Recipe</button></a>
+
+  <br>
+  <br>
+   <a href="index.php"><button  class="btn btn-danger""> Logout</button></a>
+
+  <div class="container">
+<div class="row">
+<div class="col-md-12">
+<?php
+if (mysqli_num_rows($result) > 0) {
+?>
+  <table id="mytable" class="table table-bordred table-striped"> 
+  
+  <tr>
+    <th class="table-header" width="20%">ID</th>
+    <th class="table-header" width="20%">Title</th>
+    <th class="table-header" width="40%">Instructions</th>
+    <th class="table-header" width="20%">Time</th>
+    <th class="table-header" width="5%">Actions</th>
+    
+  </tr>
+<?php
+$i=0;
+while($row = mysqli_fetch_array($result)) {
+?>
+<tr class="table-row">
+    
+	   <td><?php echo $row["id"]; ?></td>
+    <td><?php echo $row["title"]; ?></td>
+    <td><?php echo $row["instructions"]; ?></td>
+    <td><?php echo $row["created_at"]; ?></td>
+   <!-- <td><a href="update.php"><button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-pencil"></span></button></a></td> -->
+   <td><a href="update-script.php?id=<?php echo $row["id"];?>"><button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-pencil"></span></button></a></td>
+   <!-- <td><a href="update-script.php?id=<?php echo $row["id"]; ?>">Update</a></td> -->
+    <td><a href="delete.php?id=<?php  echo $row["id"];?>"><button class="btn btn-danger btn-xs" onClick="return confirm('Proceed to delete');"><span class="glyphicon glyphicon-trash"></span></button></a></td>
+    </tr>
+   
+</tr>
+<?php
+$i++;
+}
+?>
+</table>
+ <?php
+}
+else{
+    echo "No result found";
+}
+?>
+ </body>
+</html>
