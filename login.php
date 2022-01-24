fc44d2b7ac801299f69c77d6d5ff1d32e36b51af
nyaata <tessnyaata@gmail.com>
2022-01-24 09:18:12 +0300

    login form

---
+++ login.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color:powderblue;">
+		<body>
+
+  <title> "Login"</title>
+
+		   <form action="access.php" method="post">
+
+		  <label for:"email">Username:</label>
+ 			<input type="text" class="col" name="email" placeholder="Please enter email" required=""><br><br><br>
+ 		</div>
+
+ 			<label for:"password">Password:</label>
+ 			<input type="password" class="col" name="password" placeholder="Input a password" required=""><br><br><br>
+ 		</div>
+		    <br>
+		    <div class="form-group">  
+            <label for="role">Role:</label>
+            <select class="col" name="role" required="">
+            	<option value="">Select Role</option>
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+             
+            </select>
+          </div>
+		    
+		    <input type="submit" name="Login" value="Login" class="btn btn-primary">
+		    </form>
+		    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- textaddneww -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:15px"
+     data-ad-client="ca-pub-8906663933481361"
+     data-ad-slot="3318815534"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>            
+
+
+					 	<a href="register.php">Create new account</a>
+
+
+		    	
+		    
+		 
+
+		 </body>
+
+
+				 
+		 
+</htlm>
+
+
