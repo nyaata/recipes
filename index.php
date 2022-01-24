8b08aca167c1fbd795e8253c1f6b71ef56762e99
nyaata <tessnyaata@gmail.com>
2022-01-24 09:17:14 +0300

    Welcome page

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Tupike</title>
+</head>
+<h2>Tupike Pamoja</h2>
+
+
+
+<!-- javascript script to notifies delete one data  -->
+<script language="JavaScript" type="text/javascript">
+function checkDelete(){
+    return confirm('Are you sure?');
+}
+</script>
+
+<!-- Color the website background -->
+<body style="background-color:powderblue;">
+
+<center>
+
+    
+</center>
+
+<title> "Login Page"</title>
+
+            <form action="access.php" method="post">
+            <label for:"email">Username:</label>
+            <input type="text" class="col" name="email" placeholder="Please enter email" required=""><br><br><br>
+        </div>
+
+            <label for:"password">Password:</label>
+            <input type="password" class="col" name="password" placeholder="Input a password" required=""><br><br><br>
+        </div>
+            <br>
+            <br>
+            <div class="form-group">  
+            <label for="role">Role:</label>
+            <select class="form-control" name="role" required="">
+                <option value="">Select Role</option>
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+             
+            </select>
+          </div>
+            
+            <input type="submit" name="Login" value="Login" class="btn btn-success">
+            </form>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- textaddneww -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:15px"
+     data-ad-client="ca-pub-8906663933481361"
+     data-ad-slot="3318815534"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>            
+
+
+                        <a href="register.php">Create new account</a>
+
+
+                
+            
+         
+
+</body>
+</html>
